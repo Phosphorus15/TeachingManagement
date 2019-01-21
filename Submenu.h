8381ae3a772a6279cdcd6ab9ee3703cbd565ad63
XQUANZI <1814536559@qq.com>
2019-01-21 23:28:23 +0800

    submenu

--- Submenu.h
+++ Submenu.h
@@ -13,105 +13,60 @@ extern "C" {
 
 using namespace console::color;
 
-const int KEY_UP = 72;
-const int KEY_DOWN = 80;
+namespace student_main {
 
-void reset1();
-void reset2();
-void reset3();
-void reset4();
+    enum type {
+        student, teacher, admin, error
+    };
+
+    type check_login(std::string username, std::string password) { return admin; }
+
+    void login() {
+        console::clearscreen();
+        std::cin.get();
+    }
+
+    const std::string options[4] = {"List Offered Course", "Choose a Course", "Browse selected course","Return"};
+
+    void submenumain() {
+        console::clearscreen();
+        std::cout << "|*********************************************************|\n"
+                     "|*                                                       *|\n"
+                     "|*                      Submenu                          *|\n"
+                     "|*                                                       *|\n"
+                     "|*                  List Offered Course                  *|\n"
+                     "|*                  Choose a Course                      *|\n"
+                     "|*                  Browse selected course               *|\n"
+                     "|*                  Return                               *|\n"
+                     "|*                                                       *|\n"
+                     "|*********************************************************|" << std::endl;
 
 
-enum type {
-    student, teacher, admin, error
-};
-
-type check_login(std::string username, std::string password) {return admin;}
-
-void login() {
-    console::clearscreen();
-
-    std::cin.get();
-}
-
-
-void mainmenu() {
-    console::clearscreen();
-    std::cout<<"|*********************************************************|\n"
-               "|*                                                       *|\n"
-               "|*                      Submenu                          *|\n"
-               "|*                                                       *|\n"
-               "|*                1. List Offered Course                 *|\n"
-               "|*                2. Choose a Course                     *|\n"
-               "|*                3. Browse selected course              *|\n"
-               "|*                0. Return                              *|\n"
-               "|*                                                       *|\n"
-               "|*********************************************************|"<<std::endl;
-
-    int ch;
-    int s = 1;
-
-    console::setcursor(19, 4);
-    std::cout << setc(black, white, false) << std::setw(2) << ' ' << resetc() << std::flush;
-    console::setcursor(19, 4);
-    while (true) {
-        ch = getch();
-        if (ch == 224) {
-            ch = getch();
-            if (ch == KEY_UP) {
-                if (s > 1) s--;
-            } else if (ch == KEY_DOWN) {
-                if (s < 4) s++;
+        int ch;
+        int s = 0;
+        while (true) {
+            console::clearscreen();
+            std::cout << text << std::endl;
+            const int x = 19;
+            for (int i = 0; i < 4; i++) {
+                console::setcursor(x, static_cast<console::u16>(4 + i));
+                if (i == s) std::cout << setc(black, white, false);
+                std::cout << options[i] << std::flush;
+                if (i == s) std::cout << resetc();
             }
-        }
-        if (s == 1) {
-            console::setcursor(19,4);
-            std::cout << setc(black, white, false) << std::setw(2) << ' ' << resetc() << std::flush;
-            reset4();
-            console::setcursor(19, 4);
-        } else if (s == 2) {
-            console::setcursor(19, 5);
-            std::cout << setc(black, white, false) << std::setw(2) << ' ' << resetc() << std::flush;
-            reset3();
-            console::setcursor(19, 5);
-        } else if (s == 3) {
-            console::setcursor(19, 6);
-            std::cout << setc(black, white, false) << std::setw(2) << ' ' << resetc() << std::flush;
-            reset2();
-            console::setcursor(19, 6);
-        } else {
-            console::setcursor(19, 7);
-            reset1();
-            console::setcursor(19, 7);
+            ch = getch();
+            if (ch == 224) {
+                ch = getch();
+                if (ch == KEY_UP) {
+                    if (s > 0) s--;
+                } else if (ch == KEY_DOWN) {
+                    if (s < 3) s++;
+                }
+            } else {
+                if (ch == '\r') {
+                    if (s == 3) exit(0);
+                }
+            }
         }
     }
 }
-
-void reset4() {
-    console::setcursor(19,7) ;
-    std::cout << std::setw(2) << ' ' << std::flush;
-}
-void reset3() {
-    console::setcursor(19, 6);
-    std::cout << std::setw(2) << ' ' << std::flush;
-}
-
-void reset2() {
-    console::setcursor(19, 5);
-    std::cout << std::setw(2) << ' ' << std::flush;
-}
-
-void reset1() {
-    console::setcursor(19, 4);
-    std::cout << std::setw(2) << ' ' << std::flush;
-}
-
-
-
-
-int main() {
-    mainmenu();
-    std::cin.get();
-    return 0;
-}
-
